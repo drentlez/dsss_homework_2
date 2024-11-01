bdd76088251abc418c7f12519ccf65d3ac987bda
Daniel Zeltner <daniel.zeltner@fau.de>
2024-11-01 18:13:13 +0100

    Finished

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -18,14 +18,15 @@ def random_Integer(min: int, max: int) -> int:
         '>>> random_Integer(0, 100)'
         8
     """
+    if min > max:
+        raise ValueError('min must be less than max')
     try:
         return random.randint(min, max)
     except TypeError:
         print('Warning: Please enter integers. Replaced the given values with Integers')
-        min = int(min)
-        max = int(max)
+        return random.randint(round(min), round(max))
 
-def random_Operator():
+def random_Operator() -> str:
     """
         Chooses a random operator of the given list containing '+', '-', '*'
 
@@ -41,7 +42,7 @@ def random_Operator():
     return random.choice(['+', '-', '*'])
 
 
-def compute(number1, number2, operator):
+def compute(number1: int | float, number2: int | float, operator: str) -> (str, int):
     """
             Computes the given operator for the Integers number1 and number2
 
@@ -62,11 +63,21 @@ def compute(number1, number2, operator):
                 '>>> compute(2,3,'*')'
                 '2 * 3', 6
             """
+    if not(isinstance(number1, int) or isinstance(number1, float)):
+        raise TypeError('Integer or float required for number1')
+    if not(isinstance(number2, int) or isinstance(number2, float)):
+        raise TypeError('Integer or float required for number2')
+    if operator not in ['+', '-', '*']:
+        raise ValueError(f'unknown operator: {operator}')
     problem = f"{number1} {operator} {number2}"
-    if operator == '+': answer = number1 + number2            #mistake: changed - with +
-    elif operator == '-': answer = number1 - number2          #mistake: changed + with -
-    else: answer = number1 * number2                          #* is the only operation left in the operation-list
+    if operator == '+':
+        answer = number1 + number2  # mistake: changed - with +
+    elif operator == '-':
+        answer = number1 - number2  # mistake: changed + with -
+    else:
+        answer = number1 * number2  # * is the only operation left in the operation-list
     return problem, answer
+
 
 def math_quiz():
     """
@@ -75,7 +86,7 @@ def math_quiz():
             :Errors:
             """
     score = 0
-    total_questions = 3            #we can only have integers as number of total questions --> change from pi to 3
+    total_questions = int(3.14159265359)            #we can only have integers as number of total questions --> change from pi to 3
 
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
@@ -85,11 +96,14 @@ def math_quiz():
         PROBLEM, ANSWER = compute(number1, number2, operator)
         print(f"\nQuestion: {PROBLEM}")         #show the participant the exercise to solve
         useranswer = input("Your answer: ")
-        useranswer = int(useranswer)            #integer needed for further ellaborations
+        try:
+            useranswer = int(useranswer)            #integer needed for further ellaborations
+        except ValueError:
+            raise ValueError('Integer input required')
 
         if useranswer == ANSWER:
             print("Correct! You earned a point.")
-            score += -(-1)                              #counting the score for every correct answer
+            score += 1                              #counting the score for every correct answer
         else:
             print(f"Wrong answer. The correct answer is {ANSWER}.")
 
