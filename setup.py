6a9f65f011a0b0966fc064394bde68c16c5915e2
Daniel Zeltner <danielzeltner04@gmail.com>
2024-11-01 22:14:07 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,11 @@
+from distutils.core import setup
+
+setup(name='Distutils',
+      version='1.2.2',
+      description='simple math quizes',
+      author='Daniel Zeltner',
+      author_email='daniel.zeltner@fau.de',
+      url='https://github.com/drentlez/dsss_homework_2',
+      packages=['distutils', 'distutils.command'],
+     )
 
