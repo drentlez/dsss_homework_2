3e0ea07dd5dfca7d7226939acf1e63c620956455
Daniel Zeltner <daniel.zeltner@fau.de>
2024-11-01 23:09:36 +0100

    Task 5/5

--- setup.py
+++ setup.py
@@ -4,20 +4,22 @@ setup(
     name="math_quiz",
     version="1.2.3pi",
     packages=find_packages(),
-    install_requires=[],  # Hier ggf. Abhängigkeiten ergänzen
+    install_requires=[
+        "setuptools~=68.1.2"
+    ],  # Hier ggf. Abhängigkeiten ergänzen
     entry_points={
         "console_scripts": [
             "math_quiz=math_quiz.math_quiz:main",
         ],
     },
-    author="Dein Name",
-    author_email="deine.email@example.com",
-    description="Ein CLI-Tool für Mathe-Übungen",
+    author="Daniel Zeltner",
+    author_email="daniel.zeltner@fau.de",
+    description="a tool for simple math questions",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/dein-benutzername/math_quiz",
+    url="https://github.com/drentlez/dsss_homework_2",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: Apache License",
     ]
 )
